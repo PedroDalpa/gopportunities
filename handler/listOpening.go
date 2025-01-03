8a15df2b7dd47e9de3517cdae39656a34e5d66d9
Pedro Dalpa <pedro.dalpa@gmail.com>
2025-01-03 15:01:24 -0300

    Add fetch openings

--- handler/listOpening.go
+++ handler/listOpening.go
@@ -3,9 +3,17 @@ package handler
 import (
 	"net/http"
 
+	"github.com/PedroDalpa/gopportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ListOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{"message": "Hello, World!"})
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error fetching openings")
+		return
+	}
+
+	sendSuccess(ctx, "list openings", openings)
 }
