ce5b55f3aa6239a4dcb57660214748930fa28cb8
Pedro Dalpa <pedro.dalpa@gmail.com>
2025-01-03 15:13:00 -0300

    Add show opening by id

--- handler/showOpening.go
+++ handler/showOpening.go
@@ -3,9 +3,24 @@ package handler
 import (
 	"net/http"
 
+	"github.com/PedroDalpa/gopportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ShowOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{"message": "Hello, World!"})
+	id := ctx.Query("id")
+
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "query parameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, "opening not found")
+		return
+	}
+
+	sendSuccess(ctx, "show opening", opening)
 }
